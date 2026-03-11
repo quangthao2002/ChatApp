6cc5f49da57e002d9ac46a83a8b634c819d014a3
quangthao2002 <quangthao22122002@gmail.com>
2026-03-11 22:21:11 +0700

    protected

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("database url:", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signUp, signIn, signOut } from "../controllers/authController.js";
+import {
+  signUp,
+  signIn,
+  signOut,
+  refreshToken,
+} from "../controllers/authController.js";
 
 const router = express.Router(); // Create a router instance
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/signout", signOut);
+router.post("/refreshtoken", refreshToken);
 
 export default router;
